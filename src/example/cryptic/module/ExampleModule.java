7bc07a70d28fe694d61bc967cd4d24d25d8e0c9d
xXJarJarXx <598Johnn897@gmail.com>
2015-03-13 14:58:43 -0400

    ExampleModule

---
+++ src/example/cryptic/module/ExampleModule.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package example.cryptic.module;
+
+import cryptic.network.module.Module;
+import cryptic.network.module.ModuleException;
+import cryptic.network.module.ModuleInfo;
+
+/**
+ * @author 598Johnn897
+ *
+ */
+@ModuleInfo(id = "example", name = "ExampleModule", version = "0.13.37")
+public class ExampleModule extends Module
+{
+
+	public ExampleModule() throws ModuleException {
+		super();
+	}
+
+	@Override
+	public void load() {
+		
+	}
+
+	@Override
+	public void enable() {
+		
+	}
+
+	@Override
+	public void disable() {
+		
+	}
+
+}
